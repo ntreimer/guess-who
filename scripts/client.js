c7e76c587a98d07836c946ae60096a96d2c6aeff
Nate Treimer <natetreimer@gmail.com>
2021-03-08 16:11:02 -0600

    added div with image of each person

--- scripts/client.js
+++ scripts/client.js
@@ -1,1 +1,15 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+function displayProfiles(array) {
+    for (person of array) {
+        $('#pictures-here').append(`
+        <div><img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}"></div>
+        `);
+    }
+}
+
+function onReady() {
+    displayProfiles(people);
+}//end onReady
